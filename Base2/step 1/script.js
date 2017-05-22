0cbec4bfc5589cbed211ffebabbb75e17219db44
Gaby Karam <gkaramgk94@gmail.com>
2017-05-22 11:35:23 +0300

    Base2_Step_1

---
+++ Base2/step 1/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by gabykaram on 5/22/17.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+    const image1 = document.getElementById('image1');
+    image1.addEventListener('mouseover', function () {
+        image1.src = "images/image1_2.jpg";
+    });
+    image1.addEventListener('mouseout', function () {
+        image1.src = "images/image1.jpg";
+    })
+});
