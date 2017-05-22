36ecc36aa6d71b6943d8bc34c04e80bfb8bfdba2
Gaby Karam <gkaramgk94@gmail.com>
2017-05-22 11:23:45 +0300

    Base1_Step_7

---
+++ Base1/step7.js
@@ -0,0 +1,10 @@
+/**
+ * Created by gabykaram on 5/22/17.
+ */
+const shoe_size = document.getElementById("shoe_size");
+const year = document.getElementById("year");
+
+validate.addEventListener('click', function () {
+    const result = ((((parseInt(shoe_size.value) *2)+5)*50)/parseInt(year.value) )+ 1766 ;
+    alert("The Result is " + result);
+});
