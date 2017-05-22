bffaf3c60a3fb0e0e0a065f2d9626c17c51b51f6
Gaby Karam <gkaramgk94@gmail.com>
2017-05-22 11:27:54 +0300

    Base1_Step_8

--- Base1/step7.js
+++ Base1/step7.js
@@ -3,6 +3,7 @@
  */
 const shoe_size = document.getElementById("shoe_size");
 const year = document.getElementById("year");
+const validate = document.getElementById("validate");
 
 validate.addEventListener('click', function () {
     const result = ((((parseInt(shoe_size.value) *2)+5)*50)/parseInt(year.value) )+ 1766 ;
