9e9d8f98df99ae0d53ba2c78271439ff76405779
koookiii <chriss.hadad@gmail.com>
2017-05-24 07:13:10 +0300

    base 6 step 4

--- Base6/index.js
+++ Base6/index.js
@@ -1,15 +1,15 @@
-
+'use strict';
 /**
  * startApp - Starts the applucation
  *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
-  console.log("Welcome to "+name+"'s application!");
+  console.log("Welcome to " + name + "'s application!");
   console.log("--------------------");
 }
 
@@ -21,19 +21,21 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n'|| text==='exit\n') {
+  if (text === 'quit\n' || text === 'exit\n') {
     quit();
-  }
-  else if(text === 'hello\n'){
+  } else if (text === 'hello\n') {
     hello();
-  }
-  else if(text === 'help\n'){
-  help();
-  }
-  else if (text.startsWith('hello ')&& text.endsWith("\n")) {
-  helloSomeone(text.slice(6,text.length-1));
-  }
-  else{
+  } else if (text === 'help\n') {
+    help();
+  } else if (text.startsWith('hello ') && text.endsWith("\n")) {
+    helloSomeone(text.slice(6, text.length - 1));
+  } else if (text.startsWith('add ') && text.endsWith("\n")) {
+    add(text.slice(4, text.length - 1));
+  } else if (text === 'list\n') {
+    list();
+  } else if (text === "remove\n") {
+    remove();
+  } else {
     unknownCommand(text);
   }
 }
@@ -45,8 +47,8 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('\nunknown command: "' + c.trim() + '"\n')
 }
 
 
@@ -55,34 +57,66 @@ function unknownCommand(c){
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
-}
-function helloSomeone(x){
-  console.log('hello '+x+"!");
+function hello() {
+  console.log('\nhello!\n')
 }
 
+function helloSomeone(x) {
+  console.log('\nhello ' + x + "!\n");
+}
+/**
+ * add - adds a task and stores it in an array
+ *
+ * @returns {void}
+ */
+function add(x) {
+  tasks.push(x);
+  console.log('\nadded task: "' + x + '"\n');
+}
+/**
+ * list - lists tasks
+ *
+ * @returns {void}
+ */
+function list() {
+  console.log("\nhere's the list of tasks:")
+  for (var i = 0; i < tasks.length; i++) {
+    console.log("-" + tasks[i]);
+  }
+console.log(' \n');
 
+}
 /**
  * quit - exits the application
  *
  * @returns {void}
  */
-function quit(){
+function quit() {
   console.log('Quitting now, goodbye!')
   process.exit();
 }
 /**
-* help - lists all possible inputs
-*
-*/
-function help(){
+ * remove - removes last taks from list
+ *
+ */
+ function remove(){
+   console.log('\nremoved task:"'+tasks.pop()+'"\n');
+ }
+/**
+ * help - lists all possible inputs
+ *
+ */
+function help() {
   console.log(
-  '\nHere is a list of all possible commands: \n'+
-  'hello - Simple Hello! \n'+
-  'hello "yourname" - shows your name with the hello\n'+
-  'quit - quits the application \nexit - exists the application  ');
+    '\nHere is a list of all possible commands: \n' +
+    'hello - Simple Hello! \n' +
+    'hello "yourname" - shows your name with the hello\n' +
+    'quit - quits the application \nexit - exists the application  \n' +
+    'add "task"- adds a task \n' +
+    'list - lists tasks  \n' +
+     'remove - removes the last task in the list \n');
 }
 
 // STARTING THE APPLICATION HERE!
+let tasks = new Array();
 startApp("Chriss Haddad")
