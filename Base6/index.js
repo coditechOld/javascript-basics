580e252e619b6551bc0a9670e4acbbcff5389097
koookiii <chriss.hadad@gmail.com>
2017-05-26 14:25:18 +0300

    finished javscript excercices

--- Base6/index.js
+++ Base6/index.js
@@ -44,28 +44,31 @@ function onDataReceived(text) {
       var index = parseInt(text.slice(5, text.length - 1));
       editModeByIndex(index);
     } else if (text === "edit\n") {
-      if (tasks.length > 0) {
-        var index = tasks.length - 1;
+      if (tasksList.length > 0) {
+        var index = tasksList.length - 1;
         editModeByEdit();
       } else {
         console.log("\nyour task list is empty\n");
       }
     } else if (text == "save\n") {
       save();
-    } else if (text.startsWith('do ') && (/\d+/.test(text.trim().slice(3, text.length - 1))) && text.endsWith("\n")) {
-      if (tasks.length > 0) {
+    }else if (text == "load\n"){
+      load();
+    }
+     else if (text.startsWith('do ') && (/\d+/.test(text.trim().slice(3, text.length - 1))) && text.endsWith("\n")) {
+      if (tasksList.length > 0) {
         var index = parseInt(text.slice(3, text.length - 1));
         done(index);
-        console.log("\ndid task " + index + " - " + tasks[index - 1].name)
+        console.log("\ndid task " + index + " - " + tasksList[index - 1].name)
       } else {
         console.log("\nyour task list is empty\n");
       }
 
     } else if (text.startsWith('undo ') && (/\d+/.test(text.trim().slice(5, text.length - 1))) && text.endsWith("\n")) {
-      if (tasks.length > 0) {
+      if (tasksList.length > 0) {
         var index = parseInt(text.slice(5, text.length - 1));
         undone(index);
-        console.log("\nundid task " + index + " - " + tasks[index - 1].name)
+        console.log("\nundid task " + index + " - " + tasksList[index - 1].name)
       } else {
         console.log("\nyour task list is empty\n")
       }
@@ -78,7 +81,7 @@ function onDataReceived(text) {
       console.log("nothing changed going back to normal mode");
       mode = "normal";
     } else {
-      tasks[index].name = text;
+      tasksList[index].name = text;
       mode = "normal";
     }
   }
@@ -118,18 +121,18 @@ function add(x) {
     name: x,
     status: "undone"
   };
-  tasks.push(task);
+  tasksList.push(task);
   console.log('\nadded task: "' + x + '"\n');
 }
 /**
- * list - lists tasks
+ * list - lists tasksList
  *
  * @returns {void}
  */
 function list() {
-  console.log("\nhere's the list of tasks:")
-  for (var i = 0; i < tasks.length; i++) {
-    console.log((i + 1) + " - " + tasks[i].name + ", status: " + tasks[i].status);
+  console.log("\nhere's the list of tasksList:")
+  for (var i = 0; i < tasksList.length; i++) {
+    console.log((i + 1) + " - " + tasksList[i].name + ", status: " + tasksList[i].status);
 
   }
   console.log(' \n');
@@ -151,63 +154,89 @@ function quit() {
  *
  */
 function remove() {
-  console.log('\nremoved task:"' + tasks.pop() + '"\n');
+  console.log('\nremoved task:"' + tasksList.pop() + '"\n');
 }
 
 function removeByIndex(x) {
-  if (x < 0 || x >= tasks.length || isNaN(x)) {
+  if (x < 0 || x >= tasksList.length || isNaN(x)) {
     console.log("\nindex not in array or you haven't entered a number\n");
   } else {
-    console.log('removed task: "' + tasks.splice(x, 1) + '"\n');
+    console.log('removed task: "' + tasksList.splice(x, 1) + '"\n');
   }
 }
 /**
- * edit - editing tasks values
+ * edit - editing tasksList values
  *
  */
 function editModeByIndex(x) {
   console.log("\nedit mode\n");
-  if (x <= tasks.length && x > 0) {
+  if (x <= tasksList.length && x > 0) {
     mode = "edit";
-    console.log("\nediting task: " + tasks[x - 1].name + "\n");
+    console.log("\nediting task: " + tasksList[x - 1].name + "\n");
   } else {
-    console.log("\nthat index is not within the number of tasks, number of tasks is: " + tasks.length)
+    console.log("\nthat index is not within the number of tasksList, number of tasksList is: " + tasksList.length)
   }
 }
 
 function editModeByEdit() {
   console.log("\nedit mode\n");
-  if (tasks.length > 0) {
+  if (tasksList.length > 0) {
     mode = "edit";
-    console.log("\nediting task: " + tasks[tasks.length - 1].name + "\n");
+    console.log("\nediting task: " + tasksList[tasksList.length - 1].name + "\n");
   }
 }
 
 function done(x) {
-  tasks[x - 1].status = "done";
+  tasksList[x - 1].status = "done";
 }
 
 function undone(x) {
-  tasks[x - 1].status = "undone";
+  tasksList[x - 1].status = "undone";
 }
 
 function save() {
-  if (tasks.length > 0) {
-    var str = JSON.stringify(tasks);
-    fs.writeFile('file.json', str, {
+  if(!process.argv[2]){
+    saveFile('file.json');
+  }
+  else{
+    saveFile(process.argv[2]);
+  }
+}
+function saveFile(name){
+  console.log('saving to',name)
+  if (tasksList.length > 0) {
+    var str = JSON.stringify(tasksList);
+    fs.writeFile(name, str, {
       encoding: 'utf8'
     }, function(err) {
       if (err) {
         console.error('there was an error: ', err);
         return;
       } else {
-        console.log('all good, thanks')
+        console.log('all good, thanks\n')
       }
     });
     console.log('\ndata was appended to file\n');
   } else {
     console.log("\nyour console is empty\n")
   }
+}
+function load(){
+  const saveName = (process.argv[2]||'file.json')
+
+  if(!process.argv[2]){
+    loadFile('file.json');
+  }
+  else{
+    loadFile(process.argv[2]);
+  }
+}
+function loadFile(name){
+  console.log("\nfile is loaded\n")
+  fs.readFile(name, 'utf8', function (err, data) {
+  if (err) throw err;
+  tasksList = JSON.parse(data);
+});
 }
 /**
  * help - lists all possible inputs
@@ -220,19 +249,20 @@ function help() {
     'hello "yourname" - shows your name with the hello\n' +
     'quit - quits the application \nexit - exists the application  \n' +
     'add "task"- adds a task \n' +
-    'list - lists tasks  \n' +
+    'list - lists tasksList  \n' +
     'remove - removes the last task in the list \n' +
     'remove "index" - removes the task at that index \n' +
     'edit - enters edit mode\n' +
     'edit "index" - enters edit mode at that index\n' +
-    'do "index" - changes a tasks status to done\n' +
-    'undo "index" - changes a tasks status to undone\n');
+    'do "index" - changes a tasksList status to done\n' +
+    'undo "index" - changes a tasksList status to undone\n'+
+  'save - saves the tasksList in a file\n');
 }
 
 // STARTING THE APPLICATION HERE!
 
 let mode = "normal";
-let tasks = new Array();
+let tasksList = new Array();
 
 
 
