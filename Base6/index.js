4dd400d3158f5a266bf3013c868f6b147aa743a0
Gaby Karam <gkaramgk94@gmail.com>
2017-05-29 10:14:55 +0300

    still need to finish the help function

--- Base6/index.js
+++ Base6/index.js
@@ -29,7 +29,8 @@ var waitingForData = false;
  */
 function onDataReceived(text) {
 
-    if (mode == 'normal') {
+    const original = text;
+    if (mode === 'normal') {
         var args = parse(text);
         console.log(args);
         text = args[0];
@@ -43,40 +44,56 @@ function onDataReceived(text) {
             console.log(hello());
         } else if (text === 'help') {
             console.log(help());
-        } else if (text === 'add' && args[1]) {
+        }
+
+
+        else if (text === 'add' && args[1]) {
             console.log("Adding Task to the list\n" +
                 "*****************\n"
             );
-            const newArray = add(task, args[1]);
-            task.length = 0;
-            task = newArray;
+            add(task, args[1]);
             console.log(task);
-        } else if (text === 'list') {
-            lists(task);
-        } else if (text === 'remove' && args[1]) {
-            remove(task, args[1]);
-        } else if (text === 'remove') {
-            remove(task);
-        } else if (text === 'edit' && args[1]) {
+        }
 
-            mode = 'edit';
+        else if (text === 'list') {
+            lists(task);
+        }
+
+        else if (text === 'remove' && args[1]) {
+            remove(task, args[1]);
+        }
+
+        else if (text === 'remove') {
+            remove(task);
+        }
+
+        else if (text === 'edit' && args[1]) {
+
+            mode = "edit";
             edit_index = args[1];
 
-        } else if (text === 'done' && args[1]) {
-            done(task, doneArray, args[1]);
-            lists(doneArray);
+        }
+        else if (text === 'done' && args[1]) {
+            done(task, args[1]);
+            lists(task);
+        } else if (text === 'undone' && args[1]) {
+            unDone(task, args[1]);
+            lists(task);
+        }
+        else if (text === 'save') {
+            save(task, args[1]);
+            lists(task);
+        } else if (text === 'load') {
+            load(args[1]);
         }
         else {
-            unknownCommand(text);
+            unknownCommand(original);
         }
-    } else if (mode == 'edit') {
-        console.log(task[edit_index]);
-        console.log(edit_index);
-        if (text !== '\n') {
-            task[edit_index] = text.substr(0, -1);
-            console.log(task[edit_index]);
-        }
+    } else if (mode === 'edit') {
 
+        task = edit(task, edit_index, text);
+
+        console.log('edit');
         mode = "normal";
     }
 }
@@ -89,6 +106,7 @@ function onDataReceived(text) {
  * @returns {void}
  */
 function unknownCommand(c) {
+
     console.log('unknown command: "' + c.trim() + '"')
 }
 
@@ -96,7 +114,7 @@ function unknownCommand(c) {
 /**
  * hello - Says hello
  * hello X - Says hello X
- *
+ * @param {string} hello
  * @returns {string}
  */
 function hello(hello) {
@@ -125,7 +143,7 @@ function quit() {
  * */
 function help() {
 
-    const string = "Base6 available command\n" +
+    return "Base6 available command\n" +
         "*****************\n" +
         "hello: Says hello\n" +
         "hello X: Says hello X!\n" +
@@ -134,66 +152,112 @@ function help() {
         "add: Adding a Task to the list\n" +
         "list: Displaying all the list\n" +
         "remove: Removing The last Tasks from the list\n";
-
-    return string;
 }
 
 /**
  * add  X- add task X to the list
+ * @param {Array} task_array
+ * @param {string} task_argument
  * @returns {Array}
  * */
 function add(task_array, task_argument) {
-    const task_variable = cloneData(task_array)
-    task_variable.unshift(task_argument);
 
-    return task_variable;
+    var taskJson = {
+        'task': task_argument,
+        'order': task.length,
+        'done': false
+    };
+    task_array.push(taskJson);
+    return task_array;
 
 }
 
 /**
  * list - Listing all Tasks
+ * @param {Array} tasks
  * @returns {void}
  * */
 function lists(tasks) {
     console.log("Listing all Tasks\n" +
         "*****************\n"
     );
+
+    function compare(a, b) {
+        if (a.order < b.order)
+            return -1;
+        if (a.order > b.order)
+            return 1;
+        return 0;
+    }
+
+    tasks.sort(compare);
+
     console.log(tasks);
 
 }
 
 /**
- * remove  - remove the last task from the list
+ * remove   - remove the last task from the list
  * @returns {int}
  * */
 function remove(task_array, args) {
-    if (parameterCheck(args)) {
-        if (/^\d+$/.test(args)) {
+    if (task_array.length !== 0) {
+
+        if (parameterCheck(args)) {
+            if (/^\d+$/.test(args)) {
 
 
-            if (args < task_array.length && args >= 0) {
-                task_array.splice(args, 1);
-                return 0;
-            } else {
-                console.log("index supported is not in the list");
-                return 1;
+                if (args < task_array.length && args >= 0) {
+                    console.log("removing task of order " + args);
+
+                    for (var i = 0; i < task_array.length; i++) {
+                        if (task_array[i].order == args) {
+                            console.log("removing task of order " + args);
+
+                            for (var j = 0; j < task_array.length; j++) {
+                                if (task_array[j].order > task_array[i].order) {
+                                    task_array[j].order--;
+                                }
+                            }
+
+                            task_array.splice(i, 1);
+                        }
+                    }
+                    // task_array.splice(args, 1);
+                    return 0;
+                } else {
+                    console.log("index supported is not in the list");
+                    return 1;
+                }
+            }
+            else {
+                console.log('argument supported ' + args + ' is not a number!')
+                return 2;
+
+
             }
         }
         else {
-            console.log('argument supported ' + args + ' is not a number!')
-            return 2;
+            console.log("Removing The last Tasks from the list\n" +
+                "*****************\n"
+            );
+
+            var i = task_array.length - 1;
+            for (var j = 0; j < task_array.length; j++) {
+                if (task_array[j].order > task_array[i].order) {
+                    task_array[j].order--;
+                }
+            }
+
+            task_array.splice(i, 1);
+
+            console.log(task);
+            console.log(i);
+            return 0;
 
         }
-    }
-    else {
-        console.log("Removing The last Tasks from the list\n" +
-            "*****************\n"
-        );
-
-        task.pop();
-        console.log(task);
-        return 0;
-
+    } else {
+        console.log("array is empty");
     }
 
 }
@@ -202,43 +266,71 @@ function remove(task_array, args) {
 /**
  * edit  - remove the last task from the list
  *      take 1 attribute that is the index of the item to be edited
- * @returns {int}
+ * @param {Array} task_array
+ * @param {Number} edit_index_arg
+ * @param {String} text_arg
+ * @returns {Array}
  * */
-function edit(task_array, args_1, args_2) {
-    if (parameterCheckNum(args_1)) {
+function edit(task_array, edit_index_arg, text_arg) {
 
-        if (/^\d+$/.test(args_1)) {
-            if (args_1 < task_array.length && args_1 >= 0) {
-                if (parameterCheck(args_2)) {
-                    task_array[args_1] = args_2;
+    console.log("still editing");
+    if ((task_array.length - 1 >= edit_index_arg) && (edit_index_arg >= 0)) {
 
+
+        for (var i = 0; i < task.length; i++) {
+
+            if (task_array[i].order == edit_index_arg) {
+                if (text_arg !== '\n') {
+                    text_arg = text_arg.substring(0, text_arg.lastIndexOf("\n"));
+                    console.log(edit_index_arg);
+                    console.log("Editing task of order " + edit_index_arg + 'with ' + text_arg);
+                    task_array[i].task = text_arg;
+                    console.log(task_array[i].order);
+
+
+                    return task_array;
+                }
+                else {
+                    console.log('Quiting without change');
                 }
 
+                break;
+
             }
-        }
-        else {
-            console.log('argument supported ' + args_1 + ' is not a number!')
 
         }
+    } else {
+        console.log('index is not in the task list');
     }
-    else {
 
-        console.log("List has not been edited");
-        console.log(task_array);
-    }
+    return task_array;
 
 }
 
 /**
  * done  - mark a task as done by adding it  to the done array
  *      take 1 attribute that is the index the need to be added to the done
+ *
+ * @param {Array} array_original
+ * @param {Number} arg
  * @returns {void}
  * */
-function done(array_original, array_toCopy, arg) {
+function done(array_original, arg) {
     if (/^\d+$/.test(arg)) {
+        console.log("2");
         if (arg < array_original.length && arg >= 0) {
-            array_toCopy[array_toCopy.length] = array_original[arg];
-            array_original.splice(arg, 1);
+            console.log("2");
+
+            for (var i = 0; i < array_original.length; i++) {
+                console.log("2");
+
+                if (task[i].order == arg) {
+                    console.log("22");
+
+                    array_original[i].done = true;
+
+                }
+            }
 
 
         } else {
@@ -255,12 +347,22 @@ function done(array_original, array_toCopy, arg) {
  *      take 1 attribute that is the index the need to be added and removed
  * @returns {void}
  * */
-function unDone(arg) {
+function unDone(array_original, arg) {
     if (/^\d+$/.test(arg)) {
-        if (arg < task.length && arg >= 0) {
-            // done[done.length] = done()[arg];
+        console.log("2");
+        if (arg < array_original.length && arg >= 0) {
+            console.log("2");
 
-            task.splice(arg, 1);
+            for (var i = 0; i < array_original.length; i++) {
+                console.log("2");
+
+                if (task[i].order == arg) {
+                    console.log("22");
+
+                    array_original[i].done = false;
+
+                }
+            }
 
 
         } else {
@@ -276,14 +378,27 @@ function unDone(arg) {
 /**
  * save  - save task array to json file
  *      take 1 attribute that is the data the need to be saved
+ *      @param { any } data
+ *      @param { String } file
  * @returns {void}
  * */
-function save(data) {
-    const string = JSON.stringify(data, null, '\t');
-    fs.writeFile('./data/task.json', string, function (err) {
-        if (err) return console.error(err);
-        console.log('done');
-    });
+function save(data, file) {
+
+
+    if (file === undefined) {
+        const string = JSON.stringify(data, null, '\t');
+        fs.writeFile('./data/task.json' + file, string, function (err) {
+            if (err) return console.error(err);
+            console.log('done');
+        });
+    }
+    else {
+        const string = JSON.stringify(data, null, '\t');
+        fs.writeFile('./data/' + file, string, function (err) {
+            if (err) return console.error(err);
+            console.log('done');
+        });
+    }
 
 
     /* if (fs.existsSync('./data/data.json')) {
@@ -297,21 +412,34 @@ function save(data) {
      }*/
 
 }
-function parameterCheckNum(param) {
-    return (typeof param === 'string');
+
+
+/**
+ * save  - save task array to json file
+ *      take 1 attribute that is the data the need to be saved
+ *      @param { String } file
+ * @returns {void}
+ * */
+function load(file) {
+
+    if (file === undefined) {
+        const content = fs.readFileSync("data/task.json");
+        task = JSON.parse(content);
+    }
+    else {
+        const content = fs.readFileSync("data/" + file);
+        task = JSON.parse(content);
+    }
 }
 
+
+/**
+ * parameterCheck  - check if parameter is undefined or empty string
+ * @param {any} param
+ * @returns {boolean}
+ * */
 function parameterCheck(param) {
     return ( param !== undefined && param !== '');
-}
-
-// Use the JSON parse to clone the data.
-function cloneData(data) {
-    // Convert the data into a string first
-    var jsonString = JSON.stringify(data);
-
-    //  Parse the string to create a new instance of the data
-    return JSON.parse(jsonString);
 }
 
 // STARTING THE APPLICATION HERE!
