413a1bd69ffaf353a3c416269c23bd3c8ef70420
Salim <salim@salim-Codi.Tech>
2017-06-22 00:33:39 +0300

    done

---
+++ Base1/Step_6.js
@@ -0,0 +1,5 @@
+  var gettings = function() {
+         var   firstnumb = parseInt(document.getElementById("first_number").value);
+         var secondnumb = parseInt(document.getElementById("second_number").value);
+         var result = parseInt(firstnumb%secondnumb)
+         alert("the result is "+ result)}
